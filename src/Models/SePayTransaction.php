5bc3c4552602d8b4b2554e7653d30c11d8f3fa37
Nguyen Tran Chung <nguyentranchung52th@gmail.com>
2023-11-28 17:05:14 +0900

    Update SePayTransaction.php

--- src/Models/SePayTransaction.php
+++ src/Models/SePayTransaction.php
@@ -5,6 +5,15 @@ namespace SePay\SePay\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * SePay\SePay\Models\SePayTransaction
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|SePayTransaction newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|SePayTransaction newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|SePayTransaction query()
+ *
+ * @mixin \Eloquent
+ */
 class SePayTransaction extends Model
 {
     use HasFactory;
